898520016166af2451542e80b88abc76691c2c95
Richard Nelson <unixabg@gmail.com>
2016-01-27 20:44:10 -0600

    Include sphinxbase-0.8 for Jasper dependency from Debian repository in scripts/jasper-install.sh.

--- scripts/jasper-installer.sh
+++ scripts/jasper-installer.sh
@@ -25,6 +25,10 @@ PATH=$PATH:/usr/local/lib/
 export PATH
 EOT
 
+echo "$(date) - Install the dependencies for Jasper included as Debian packages..." >> ~/jasper-installer.log
+# Install the dependencies  for Jasper included as Debian packages
+sudo apt-get install libsphinxbase1 --yes
+
 echo "$(date) - Install the dependencies and some other packages..." >> ~/jasper-installer.log
 # Install the dependencies and some other packages
 sudo apt-get install vim git-core python-dev python-pip bison libasound2-dev libportaudio-dev python-pyaudio espeak subversion autoconf libtool automake gfortran g++ --yes
@@ -34,7 +38,6 @@ echo "$(date) - Download and extract packages for STT..." >> ~/jasper-installer.
 # The Pocketsphinx STT engine requires the MIT Language Modeling Toolkit,
 # m2m-aligner, Phonetisaurus and OpenFST
 cd ~
-wget http://downloads.sourceforge.net/project/cmusphinx/sphinxbase/0.8/sphinxbase-0.8.tar.gz
 wget http://downloads.sourceforge.net/project/cmusphinx/pocketsphinx/0.8/pocketsphinx-0.8.tar.gz
 wget http://distfiles.macports.org/openfst/openfst-1.3.4.tar.gz
 wget https://mitlm.googlecode.com/files/mitlm-0.4.1.tar.gz
@@ -43,7 +46,6 @@ wget https://phonetisaurus.googlecode.com/files/is2013-conversion.tgz
 wget https://www.dropbox.com/s/kfht75czdwucni1/g014b2b.tgz
 svn co https://svn.code.sf.net/p/cmusphinx/code/trunk/cmuclmtk/
 git clone https://github.com/jasperproject/jasper-client.git jasper
-tar xvf sphinxbase-0.8.tar.gz
 tar xvf pocketsphinx-0.8.tar.gz
 tar xvf m2m-aligner-1.2.tar.gz
 tar xvf openfst-1.3.4.tar.gz
@@ -52,13 +54,6 @@ tar xvf mitlm-0.4.1.tar.gz
 tar xvf g014b2b.tgz
 
 # Install Speech-To-Text Engine Pocketsphinx and CMUCLMTK
-echo "$(date) - Building sphinxbase-0.8..." >> ~/jasper-installer.log
-cd ~/sphinxbase-0.8/
-./configure --enable-fixed
-make -j2
-sudo make install
-echo "$(date) - Completed building and installing sphinxbase-0.8..." >> ~/jasper-installer.log
-
 echo "$(date) - Building pocketshinx-0.8..." >> ~/jasper-installer.log
 cd ~/pocketsphinx-0.8/
 ./configure
